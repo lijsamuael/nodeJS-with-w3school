07fbe4c525939498074b2f26fde80a6d7a2ef7bd
Lijsamuael <lijsamuael@gmail,com>
2022-06-04 22:27:27 +0300

    creating a module and using it in the another node js file

--- node.js
+++ node.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fm = require('./module');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello World!');
+    res.write('The date and the time currently are' + fm.myDateTime());
 }).listen(8080);
