bea4a34512abd52a32d3ee4ae4d6239486129ddf
Lijsamuael <lijsamuael@gmail,com>
2022-06-05 18:19:27 +0300

    creating html file that is to be read by node js as a file.

--- node.js
+++ node.js
@@ -3,6 +3,7 @@ var fm = require('./module');
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World!');
-    res.write('The date and the time currently are' + fm.myDateTime());
+    res.write('The date and the time currently are' + fm.myDateTime())
+    res.write(req.url);
+    res.end('Hello World! ena min yitebes');
 }).listen(8080);
