4791cdd207d772e7b125e7043fd21096af1a401a
Lijsamuael <lijsamuael@gmail,com>
2022-06-05 19:05:34 +0300

    creating a .txt file using fs.open() method

--- node.js
+++ node.js
@@ -1,9 +1,28 @@
 var http = require('http');
 var fm = require('./module');
+var fs = require('fs');
 
 http.createServer(function(req, res) {
+    //reading a html file from the same directory.
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+    /*working with modules
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('The date and the time currently are' + fm.myDateTime())
     res.write(req.url);
     res.end('Hello World! ena min yitebes');
+    */
 }).listen(8080);
+
+//creating a file 
+
+fs.appendFile(
+    'newfile.txt', 'Hello This is a file created by node js file!',
+    function(err) {
+        if (err) throw err;
+        console.log('Saved!');
+    }
+);
