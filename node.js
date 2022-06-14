9ecc03018cf61ee2f62ab896ce4c41197920403e
Lijsamuael <lijsamuael@gmail,com>
2022-06-14 23:52:00 +0300

    uploading a file

--- node.js
+++ node.js
@@ -93,10 +93,37 @@ http.createServer(
 
 */
 
+/*
 //using upeer_case module
 uc = require('upper-case');
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(uc.upperCase("I am Samuel Ketema. And i am tring to use the upper-case module."));
     res.end;
+}).listen(7070);
+*/
+
+// uploading
+var formidable = require('formidable');
+var fs = require('fs');
+http.createServer(function(req, res) {
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            var oldpath = files.filetoupload.filepath;
+            var newpath = 'A:/Educational/Books/' + files.filetoupload.originalFilename;
+            fs.rename(oldpath, newpath, function(err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('< form action = "fileupload" method = "post" enctype = "multipart/form-data" >');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
 }).listen(7070);
