08a386bb72a67d66078fe9425feb11fdd528538d
Lijsamuael <lijsamuael@gmail,com>
2022-06-14 22:13:07 +0300

    using upper-case module

--- node.js
+++ node.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var fm = require('./module');
 var fs = require('fs');
-
+var url = require('url');
+/*
 http.createServer(function(req, res) {
     //reading a html file from the same directory.
     fs.readFile('index.html', function(err, data) {
@@ -15,7 +16,9 @@ http.createServer(function(req, res) {
     res.write(req.url);
     res.end('Hello World! ena min yitebes');
     */
-}).listen(8080);
+//}).listen(8080);
+
+
 
 /*creating a file 
 
@@ -61,8 +64,8 @@ fs.unlink(
 */
 
 //*********************working with url module************************************* */
+/*extracting info from url
 
-var url = require('url');
 var adr = 'https://localhost:8080/index.html?name=Samuael&age=22';
 var q = url.parse(adr, true);
 console.log(q.host);
@@ -71,3 +74,29 @@ console.log(q.search);
 var qdata = q.query;
 console.log(qdata.name);
 console.log(qdata.age);
+*/
+
+/* 
+http.createServer(
+    function(req,res){
+        var q = url.parse(req.url, true);
+        var filname = '.' + q.pathname;
+        fs.readFile(filename,
+            function(err, data){
+                if(err){
+                    res.writeHead(400,{})
+                }
+            }
+            )
+    }
+);
+
+*/
+
+//using upeer_case module
+uc = require('upper-case');
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(uc.upperCase("I am Samuel Ketema. And i am tring to use the upper-case module."));
+    res.end;
+}).listen(7070);
