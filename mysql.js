b4b7265c765d2888d6f6703e2b673ac19b3cffcf
Lijsamuael <lijsamuael@gmail,com>
2022-06-17 13:22:17 +0300

    creating a table

--- mysql.js
+++ mysql.js
@@ -1,18 +1,35 @@
 var mysql = require('mysql');
 //creating connection to the database
-/*
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "sami",
-    password: "password"
+    password: "password",
+    database: "nodedb"
+
 });
-*/
-// creating a database
+
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected to Database");
+    // creating a database
+
+    /*
     con.query("CREATE DATABASE nodedb", function(err, result) {
         if (err) throw err;
         console.log("Database named nodedb created");
     });
+    */
+
+    //creating a table
+
+    var sql = "CREATE TABLE Student (id int auto_increment primary key, name varchar(20), age varchar(1))";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("A table named Student created.");
+    });
+
+
+
+
 });
