eca9bf206651628dd91625fb18244e0e27c1e20b
Lijsamuael <lijsamuael@gmail,com>
2022-06-19 12:09:28 +0300

    creating a collection

--- mongodb.js
+++ mongodb.js
@@ -2,5 +2,9 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017";
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    console.log("Connected to Mongodb database");
+    var dbo = db.db("nodedb")
+    dbo.createCollection("Department", function(err, res) {
+        if (err) throw err;
+        console.log("A Department Collection created in nodedb database")
+    });
 });
